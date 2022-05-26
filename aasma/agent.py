1a8bef1c014f4735c4dd07beb5acb2eaa386cfca
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2022-05-26 11:06:03 +0100

    Added observations to agent

--- aasma/agent.py
+++ aasma/agent.py
@@ -1,47 +1,74 @@
+from abc import abstractmethod, ABC
+
+import pygame
+import random
 import numpy as np
-from abc import ABC, abstractmethod
+
+prey_view_range = 3
+seeker_view_range = 2
 
 
 class Agent(ABC):
+    def __init__(self, row, col, is_prey: bool):
+        self.team_positions = None
+        self.visible_enemy_positions = None
+        self.current_position = np.array((row, col))
+        self._is_prey = is_prey
+        self.eliminated = False
+        self.direction = None
+        self.view_range = prey_view_range if is_prey else seeker_view_range
 
-    """
-    Base agent class.
-    Represents the concept of an autonomous agent.
+    def is_prey(self):
+        return self._is_prey
 
-    Attributes
-    ----------
-    name: str
-        Name for identification purposes.
-        
-    observation: np.ndarray
-       The most recent observation of the environment
+    def is_seeker(self):
+        return not self._is_prey
 
+    def eliminate(self):
+        self.eliminated = True
 
-    Methods
-    -------
-    see(observation)
-        Collects an observation
+    def move(self):
+        decision = random.randint(0, 3)
+        print(decision)
 
-    action(): int
-        Abstract method.
-        Returns an action, represented by an integer
-        May take into account the observation (numpy.ndarray).
+        if decision == 0:
+            self.col = self.col - 1
 
-    References
-    ----------
-    ..[1] Michael Wooldridge "An Introduction to MultiAgent Systems - Second
-    Edition", John Wiley & Sons, p 44.
+        elif decision == 1:
+            self.row = self.row + 1
 
+        elif decision == 2:
+            self.col = self.col + 1
 
-    """
+        elif decision == 3:
+            self.row = self.row - 1
 
-    def __init__(self, name: str):
-        self.name = name
-        self.observation = None
+    def receive_status(self, prey_positions: np.ndarray, seekers_positions: np.ndarray):
+        """Receives the current status of the board and calls see() method with only what
+        this agent can see"""
+        if self.is_prey():
+            team_positions = prey_positions
+            enemy_positions = seekers_positions
+        else:
+            team_positions = seekers_positions
+            enemy_positions = prey_positions
 
-    def see(self, observation: np.ndarray):
-        self.observation = observation
+        visible_enemy_positions = []
+
+        for i in range(len(enemy_positions)):
+            # Filtering the enemy positions that this agent can see
+            if (np.abs(enemy_positions[i] - self.current_position) <= self.view_range).all():
+                visible_enemy_positions.append(enemy_positions[i])
+
+        self.see(team_positions, np.array(visible_enemy_positions))
+
+    def see(self, team_positions: np.ndarray, visible_enemy_positions: np.ndarray):
+        self.team_positions = team_positions
+        self.visible_enemy_positions = visible_enemy_positions
 
     @abstractmethod
     def action(self) -> int:
         raise NotImplementedError()
+
+    def __repr__(self):
+        return str("Prey" if self.is_prey() else "Seeker")
